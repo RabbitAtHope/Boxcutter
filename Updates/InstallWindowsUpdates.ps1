f9fdb347f23b0c6d85cf6512012173f41f64b10c
Madeleine Rabitoy <rabitoy@hope.edu>
2024-05-03 10:46:53 -0400

    Update InstallWindowsUpdates.ps1

--- Updates/InstallWindowsUpdates.ps1
+++ Updates/InstallWindowsUpdates.ps1
@@ -1,10 +1,4 @@
 try {
-	"⏳ Checking requirements..."
-	& "$PSScriptRoot/check-smart-devices.ps1"
-	& "$PSScriptRoot/check-drive-space.ps1" C
-	& "$PSScriptRoot/check-swap-space.ps1"
-	& "$PSScriptRoot/check-pending-reboot.ps1"
-	Start-Sleep -seconds 3
 	"⏳ Installing updates..."
 	& winget upgrade --all --include-unknown
 	"✅ Updates installed."

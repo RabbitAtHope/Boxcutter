3138df1a2952560a859474f0b6e1d932bcc03e69
Madeleine Rabitoy <rabitoy@hope.edu>
2024-05-07 14:06:18 -0400

    Turns out this function no longer works

--- Maintenance/EmptyRecycleBin.ps1
+++ Maintenance/EmptyRecycleBin.ps1
@@ -1,12 +1,5 @@
 try {
-    Write-Output "⏳ Checking if the recycle bin has any items..."
-    $recycleBinItems = Get-RecycleBin
-    if ($recycleBinItems.Count -eq 0) {
-        Write-Output "✅️ Recycle bin is already empty."
-        exit 0
-    }
-	
-	Write-Output "⏳ Cleaning recycle bin on all drives..."
+    Write-Output "⏳ Cleaning recycle bin on all drives..."
 	Clear-RecycleBin -Confirm:$false
 	Write-Output "✅ Recycle bin emptied."
 	exit 0
